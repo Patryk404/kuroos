03f8b48fe8b8ca41d449d88e3aa6f7a67a66a94e
Patryk404 <patryk744@op.pl>
2023-02-27 10:51:11 +0100

    Put char

--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,60 @@
 #include "kernel.h"
 
+void clear_screen();
+int strlen();
+void put_char(char c, char colour);
+
+void print(const char *string, int length);
 void kernel_main()
 {
-    uint8_t *p = (uint8_t *)0xb8000;
-    *p = 'D';
-    p++;
-    *p = 2;
+    clear_screen();
+    put_char('A', GREEN_VGA_COLOUR);
+    put_char('B', GREEN_VGA_COLOUR);
+
+    // print("siema siema", strlen("siema siema"));
+}
+
+void clear_screen()
+{
+    uint8_t *screen_ptr = (uint8_t *)VGA_DISPLAY_ADDRESS;
+    for (int i = 0; i < 2048; i++)
+    {
+        screen_ptr[i] = 0;
+    }
+}
+
+void print(const char *string, int length)
+{
+    uint8_t *screen_ptr = (uint8_t *)VGA_DISPLAY_ADDRESS;
+    // screen_ptr[0] = 'a';
+    // screen_ptr[1] = 2;
+    screen_ptr[0] = *string;
+    screen_ptr[1] = 2;
+    // int screen_ptr_counter = 0;
+    for (int i = 0; i < 12; i++)
+    {
+        // screen_ptr[screen_ptr_counter] = (uint8_t)string[i];
+        // screen_ptr_counter++;
+        // screen_ptr[screen_ptr_counter] = 2;
+        // screen_ptr_counter++;
+    }
+}
+
+void put_char(char c, char colour)
+{
+    uint8_t *screen_ptr = (uint8_t *)VGA_DISPLAY_ADDRESS;
+    screen_ptr[terminal_buffer] = c;
+    screen_ptr[terminal_buffer + 1] = colour;
+    terminal_buffer += 2;
+}
+
+int strlen(const char *string)
+
+{
+    int len = 0;
+    for (int i = 0; string[i] != '\0'; i++)
+    {
+        len++;
+    }
+    return len;
 }
--- src/kernel.h
+++ src/kernel.h
@@ -2,6 +2,9 @@
 
 #ifndef KERNEL_H
 #define KERNEL_H
-void kernel_main();
+#define VGA_DISPLAY_ADDRESS 0xb8000
+#define GREEN_VGA_COLOUR 0x02
 
+void kernel_main();
+int terminal_buffer = 0;
 #endif
